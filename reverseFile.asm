1b0ec43eab5f6cfc7ce425792b4fa9373c4d69dd
fffeiip <felipe.escorel@gmail.com>
2021-01-22 23:31:44 -0300

    finalizando versão inicial

--- reverseFile.asm
+++ reverseFile.asm
@@ -1,14 +1,15 @@
 .data
 
-#1024 é o limite de caracteres também
+#1024 é o limite de caracteres 
 output:	.space	1024
 #o caminho do arquivo tem que ir todo pra funcionar
-fileName: .asciiz "/home/felipe/Documents/Workspace/ArquiteturaComputadores/mundo-reverso-assembly/teste.txt"      # filename for input
-fileOutputName: .asciiz "/home/felipe/Documents/Workspace/ArquiteturaComputadores/mundo-reverso-assembly/output.txt"      # filename for output
-fileWords: .space 1024
+fileName: .asciiz"/home/felipe/Documents/Workspace/ArquiteturaComputadores/mundo-reverso-assembly/teste.txt"      
+fileOutputName: .asciiz "/home/felipe/Documents/Workspace/ArquiteturaComputadores/mundo-reverso-assembly/output.txt"     
+
+fileWords: .space 1024 #buffer
 
 .text
-.globl main
+
 main:
 	open_file:
 		li   $v0, 13       # codigo de abrir arquivo
@@ -39,20 +40,20 @@ reverse:
 		
 		beqz	$t4, writeFile		# chegou no final
 		
-		blt $t4, 'A', checkSpace  # < A, não é uma letra
-		nop	
+		blt $t4, 'A', checkSpace  # < A, não é uma letra.  Casos especiais 'Ç', 'ã', etc entram aqui, por não ter suporte à unicode o simulador lê um valor negativo
+			
 		ble $t4, 'Z', isUpper #  >=A e <=Z, é uma letra maiuscula
-		nop
+		
 		blt $t4, 'a', checkSpace #  < a, não é uma letra
-		nop
+		
 		ble $t4, 'z', isLower #  >=a e <=z, é uma letra minuscula
-		nop	
-		b save # default case (quando n é letra ou caracter especial)
-		nop
+			
+		b save # default case (casos especiais 'Ç', 'ã', etc cairiam aqui se tivesse suporte no mars) 
+		
 	isUpper:
 		addi $t4, $t4, 32 # converte pela tabela ascii maiuscula para minuscula
 		b save 
-		nop
+		
 	isLower:
 		addi $t4, $t4, -32 # converte pela tabela ascii minuscula pra maiuscula
 	save:
@@ -60,6 +61,7 @@ reverse:
 		subi	$t1, $t1, 1		# subtrai o contador de tamanho da string
 		addi	$t0, $t0, 1		# contador + 1
 		j	reverse_loop		# jump pra label loop
+	
 	checkSpace:
 		beq $t4, ' ', save		# se for espaço em branco salva e volta pro loop
 		add $t1, $t4, $zero
@@ -76,7 +78,7 @@ writeFile:
 	li $v0,15
 	move $a0,$s1
 	la $a1, output
-	la $a2, 0($t0)  #carrega tamanho do texto
+	la $a2, 0($t0)  #carrega tamanho do texto (pra saber onde termina e não imprimir lixo de memória)
 	syscall
 	
 	#fecha arquivo
@@ -84,11 +86,6 @@ writeFile:
 	move $a0, $s1
 	syscall
 exit:
-	imprime:
-		li	$v0, 4			
-		la	$a0, output		
-		syscall
-		
 	li	$v0, 10			
 	syscall
 
@@ -97,19 +94,21 @@ strlen:
 	li	$t2, 0
 	
 	strlen_loop:
-		add	$t2, $a0, $t0
+		add	$t2, $a0, $t0 #carrega infos do array para contagem
 		lb	$t1, 0($t2)		
 
 		beqz	$t1, strlen_exit
-		beq $t1, 0x20, counter_loop		
-		blt $t1, 'A', strlen_exit
+		beq $t1, 0x20, counter_loop #Verifica se é espaço em branco, precisa redirecionar pois 0x20 é menor que 'A'.
+		blt $t1, 'A', strlen_exit # Se for menor que A é condição de parada do sistema, então para a contagem
 
 		counter_loop:
-			addiu	$t0, $t0, 1
+			addiu	$t0, $t0, 1 #Incrementa contador
 			j	strlen_loop
 
 	strlen_exit:
-		subi	$t0, $t0, 1
-		add	$v0, $zero, $t0
-		add	$t0, $zero, $zero
+		subi	$t0, $t0, 1  #subtrai 1 da contagem pra pegar o total
+		add	$v0, $zero, $t0 #salva contagem
+		add	$t0, $zero, $zero #limpa t0
 		jr	$ra
+
+		
